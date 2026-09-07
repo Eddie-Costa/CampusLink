861d0cbc26cdc8ffe79d6929b50c7cd0a98536a4
Eddie Alencar Costa <eddiealencarcosta@gmail.com>
2026-09-07 01:28:06 -0300

    fim criacao esqueleto sistema

--- CampusLink/src/main/java/com/example/CampusLink/dao/alunoDAO.java
+++ CampusLink/src/main/java/com/example/CampusLink/dao/alunoDAO.java
@@ -1,7 +1,5 @@
 package com.example.CampusLink.dao;
 
-import com.example.CampusLink.dto.Aluno.loginAlunoDTO;
-import com.example.CampusLink.dto.Professor.loginProfessorDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -19,45 +17,4 @@ public class alunoDAO {
     @Autowired
     private DataSource dataSource;
 
-    public loginAlunoDTO buscarPorEmailAluno(String email) throws SQLException {
-
-        String sql = "SELECT * FROM public.\"USUARIOS\" WHERE \"email\" = ?";
-        Connection conn = dataSource.getConnection();
-        PreparedStatement stmt = conn.prepareStatement(sql);
-        stmt.setString(1, email);
-        ResultSet rs = stmt.executeQuery();
-
-        if (rs.next()) {
-            loginAlunoDTO usuario = new loginAlunoDTO();
-            usuario.setEmail(rs.getString("EMAIL"));
-            usuario.setSenha(rs.getString("SENHA"));
-
-            return usuario;
-        }
-
-        rs.close();
-        stmt.close();
-        conn.close();
-        return null;
-    }
-
-    public String buscarPorIDProfessor(String email) throws SQLException {
-
-        String  id = "";
-
-        String sql = "SELECT id FROM public.\"ALUNOS\" WHERE \"email\" = ?";
-        Connection conn = dataSource.getConnection();
-        PreparedStatement stmt = conn.prepareStatement(sql);
-        stmt.setString(1, email);
-        ResultSet rs = stmt.executeQuery();
-
-        if (rs.next()) {
-            id = rs.getString("id");
-        }
-
-        rs.close();
-        stmt.close();
-        conn.close();
-        return id;
-    }
 }
--- CampusLink/src/main/java/com/example/CampusLink/dao/professorDAO.java
+++ CampusLink/src/main/java/com/example/CampusLink/dao/professorDAO.java
@@ -1,7 +1,6 @@
 package com.example.CampusLink.dao;
 
-import com.example.CampusLink.dto.Aluno.loginAlunoDTO;
-import com.example.CampusLink.dto.Professor.loginProfessorDTO;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -19,45 +18,4 @@ public class professorDAO {
     @Autowired
     private DataSource dataSource;
 
-    public loginProfessorDTO buscarPorEmailProfessor(String email) throws SQLException {
-
-        String sql = "SELECT * FROM public.\"USUARIOS\" WHERE \"email\" = ?";
-        Connection conn = dataSource.getConnection();
-        PreparedStatement stmt = conn.prepareStatement(sql);
-        stmt.setString(1, email);
-        ResultSet rs = stmt.executeQuery();
-
-        if (rs.next()) {
-            loginProfessorDTO usuario = new loginProfessorDTO();
-            usuario.setEmail(rs.getString("EMAIL"));
-            usuario.setSenha(rs.getString("SENHA"));
-
-            return usuario;
-        }
-
-        rs.close();
-        stmt.close();
-        conn.close();
-        return null;
-    }
-
-    public String buscarPorIDProfessor(String email) throws SQLException {
-
-        String  id = "";
-
-        String sql = "SELECT p.id FROM public.\"PROFESSORES\" p\n" + "JOIN public.\"USUARIOS\" u ON p.id_usuario = u.id WHERE u.email = ?";
-        Connection conn = dataSource.getConnection();
-        PreparedStatement stmt = conn.prepareStatement(sql);
-        stmt.setString(1, email);
-        ResultSet rs = stmt.executeQuery();
-
-        if (rs.next()) {
-            id = rs.getString("id");
-        }
-
-        rs.close();
-        stmt.close();
-        conn.close();
-        return id;
-    }
 }
