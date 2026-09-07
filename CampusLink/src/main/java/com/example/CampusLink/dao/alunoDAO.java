65413ebe04200fa7c582f1831c773b90a585bbe3
Matheus Santhiago <matheus_santhiago2012@hotmail.com>
2026-09-07 20:23:20 -0300

    Ajuste no AlunoDAO

--- CampusLink/src/main/java/com/example/CampusLink/dao/alunoDAO.java
+++ CampusLink/src/main/java/com/example/CampusLink/dao/alunoDAO.java
@@ -1,7 +1,6 @@
 package com.example.CampusLink.dao;
 
 import com.example.CampusLink.dto.Aluno.loginAlunoDTO;
-import com.example.CampusLink.dto.Professor.loginProfessorDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -10,8 +9,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 @Repository
 public class alunoDAO {
@@ -19,45 +16,62 @@ public class alunoDAO {
     @Autowired
     private DataSource dataSource;
 
-    public loginAlunoDTO buscarPorEmailAluno(String email) throws SQLException {
+    public loginAlunoDTO buscarPorEmailAluno(String email)
+            throws SQLException {
 
-        String sql = "SELECT * FROM public.\"USUARIOS\" WHERE \"email\" = ?";
-        Connection conn = dataSource.getConnection();
-        PreparedStatement stmt = conn.prepareStatement(sql);
-        stmt.setString(1, email);
-        ResultSet rs = stmt.executeQuery();
+        String sql = """
+                SELECT "email", "senha"
+                FROM public."USUARIOS"
+                WHERE "email" = ?
+                """;
 
-        if (rs.next()) {
-            loginAlunoDTO usuario = new loginAlunoDTO();
-            usuario.setEmail(rs.getString("EMAIL"));
-            usuario.setSenha(rs.getString("SENHA"));
+        try (
+                Connection conn = dataSource.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql)
+        ) {
+            stmt.setString(1, email);
 
-            return usuario;
+            try (ResultSet rs = stmt.executeQuery()) {
+
+                if (rs.next()) {
+                    loginAlunoDTO usuario = new loginAlunoDTO();
+
+                    usuario.setEmail(rs.getString("email"));
+                    usuario.setSenha(rs.getString("senha"));
+
+                    return usuario;
+                }
+            }
         }
 
-        rs.close();
-        stmt.close();
-        conn.close();
         return null;
     }
 
-    public String buscarPorIDAluno(String email) throws SQLException {
+    public String buscarPorIDAluno(String email)
+            throws SQLException {
 
-        String  id = "";
+        String sql = """
+                SELECT a."id"
+                FROM public."ALUNOS" a
+                INNER JOIN public."USUARIOS" u
+                    ON u."id" = a."id_usuario"
+                WHERE u."email" = ?
+                """;
 
-        String sql = "SELECT id FROM public.\"ALUNOS\" WHERE \"email\" = ?";
-        Connection conn = dataSource.getConnection();
-        PreparedStatement stmt = conn.prepareStatement(sql);
-        stmt.setString(1, email);
-        ResultSet rs = stmt.executeQuery();
+        try (
+                Connection conn = dataSource.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql)
+        ) {
+            stmt.setString(1, email);
 
-        if (rs.next()) {
-            id = rs.getString("id");
+            try (ResultSet rs = stmt.executeQuery()) {
+
+                if (rs.next()) {
+                    return rs.getString("id");
+                }
+            }
         }
 
-        rs.close();
-        stmt.close();
-        conn.close();
-        return id;
+        return "";
     }
 }
